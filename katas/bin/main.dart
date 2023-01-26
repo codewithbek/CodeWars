d91481589697e09dee7e137304cb63bb47633ac8
Asadbek <106092435+AsadbekMobileDeveloper@users.noreply.github.com>
2023-01-26 11:37:46 +0500

    One kata solved

--- katas/bin/main.dart
+++ katas/bin/main.dart
@@ -1,5 +1,6 @@
 import 'fundamentals/8kyu/better_than_average.dart';
+import 'fundamentals/8kyu/move.dart';
 
 void main(List<String> arguments) {
-  print(betterThanAverage([29, 55, 74, 60, 11, 90, 67, 28], 21));
+  print(move(0,4));
 }
