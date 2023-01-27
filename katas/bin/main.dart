62daccd905247a4c1c2a70ce611c09167a91fb99
Asadbek <106092435+AsadbekMobileDeveloper@users.noreply.github.com>
2023-01-27 16:56:58 +0500

    One kata solved

--- katas/bin/main.dart
+++ katas/bin/main.dart
@@ -1,6 +1,7 @@
+import 'arrays/8kyu/grow.dart';
 import 'fundamentals/8kyu/better_than_average.dart';
 import 'fundamentals/8kyu/move.dart';
 
 void main(List<String> arguments) {
-  print(move(0,4));
+  print(grow([1,2,3,4]));
 }
